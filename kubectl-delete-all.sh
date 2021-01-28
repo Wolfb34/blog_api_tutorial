1da6fe5a27d581bdb58dfeeb79e623b155ff61b3
Wolf bij 't Vuur <wolfatthefire@hotmail.com>
2021-01-28 12:40:25 +0100

    add Ingress and TLS Certificate

--- kubectl-delete-all.sh
+++ kubectl-delete-all.sh
@@ -1,6 +1,6 @@
 # !/bin/bash
 
-# Delete the blogdb deployments and services and such 
+# Delete the blogdb deployments and services and such
 microk8s kubectl delete -f blogdb-storage.yaml
 microk8s kubectl delete -f blogdb-config.yaml
 microk8s kubectl delete -f blogdb-secret.yaml
@@ -14,4 +14,4 @@ microk8s kubectl delete -f flask-api-service.yaml
 # Delete blog-ui stuff
 microk8s kubectl delete -f blog-ui-deployment.yaml
 microk8s kubectl delete -f blog-ui-service.yaml
-
+microk8s kubectl delete -f blog-ui-ingress.yaml
